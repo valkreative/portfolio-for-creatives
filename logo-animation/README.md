d0383902c65aae174eeb5efa97c552ab21c22aaa
valkreative <valentinakruglikovsk@my.capilanou.ca>
2023-09-21 15:51:13 -0700

    updates

---
+++ logo-animation/README.md
@@ -0,0 +1,5 @@
+# Loading A Random Logo with Pure JavaScript (Logo Array)
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/valentinak/pen/rNopRoJ](https://codepen.io/valentinak/pen/rNopRoJ).
+
+This is my attempt at loading random images with JavaScript. Now, I am still a novice when it comes to JavaScript so this is not an attempt to educate anybody. This is just me trying to apply JavaScript in real world scenarios. If there is any constructive criticism to share please do so. Please do not have professional level expectations and then bash me /my code. I'm still learning.
---
+++ logo-animation/dist/script.js
@@ -0,0 +1,34 @@
+function randomImage(element) {
+	const el = element.children[0];
+	// 	Array of Image classes
+	const imageClasses = [
+		[
+			"logo-version-1",
+			"logo-version-2",
+			"logo-version-3",
+			"logo-version-4"
+		]
+	];
+
+	// 	getting an array of images for each letter based on the id of the anchor element clicked
+	let randomImageClasses;
+	const elementID = element.getAttribute("id");
+	if (elementID === "logo-1") {
+		randomImageClasses = imageClasses[0];
+	} else {
+		randomImageClasses = imageClasses[1];
+	}
+	// 	get the number of images
+	const numberOfImages = randomImageClasses.length;
+	// 	choose a random image that is not bigger than the number of images
+	const randomImage = Math.floor(Math.random() * numberOfImages);
+	// choose another image in case the random image is the current
+	const anotherImage =
+		numberOfImages == randomImage + 1 ? randomImage - 1 : randomImage + 1;
+	// if the random image is the current image then choose another, else choose the random image
+	const chosenImage =
+		el.className === randomImageClasses[randomImage]
+			? randomImageClasses[anotherImage]
+			: randomImageClasses[randomImage];
+	el.className = chosenImage;
+}
